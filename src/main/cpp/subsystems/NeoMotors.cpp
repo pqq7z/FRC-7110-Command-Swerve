2297e8e87c633398db04cc40dd86fbdd21d7ebe0
Nathan C <correanathanh@gmail.com>
2022-08-25 07:44:17 -0700

    Method to set rpm in class FRC7110::NeoMotor

--- src/main/cpp/subsystems/NeoMotors.cpp
+++ src/main/cpp/subsystems/NeoMotors.cpp
@@ -3,7 +3,11 @@
 using namespace FRC7110;
 
 NeoMotor::NeoMotor(const int Id, rev::CANSparkMax::MotorType type) : 
-rev::CANSparkMax(Id, type), rev::SparkMaxRelativeEncoder(GetEncoder()){}
+rev::CANSparkMax(Id, type), rev::SparkMaxRelativeEncoder(GetEncoder()){
+  if (GetIdleMode() != rev::CANSparkMax::IdleMode::kCoast) {
+  SetIdleMode(rev::CANSparkMax::IdleMode::kCoast); 
+  BurnFlash();}
+}
 
 void NeoMotor::SetRPM2MPS(const double Ratio) {
   m_Ratio = Ratio;
@@ -13,4 +17,12 @@ double NeoMotor::GetRate() {
   double mps = (GetVelocity()/60) * m_Ratio;
 
   return mps;
+}
+
+void NeoMotor::SetRPMBB(double RPM) {
+  Set(m_controller.Calculate(GetVelocity(), RPM));
+}
+
+void NeoMotor::SetRPMpid(double RPM) {
+  Set(m_PID.Calculate(GetVelocity(), RPM));
 }
